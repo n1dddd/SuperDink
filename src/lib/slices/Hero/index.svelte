13a6773a2449e05ffb30c4455bff4554597256c7
n1dddd <danielseniv@outlook.com>
2024-07-10 19:00:43 -0400

    First Commit, initialize demo

---
+++ src/lib/slices/Hero/index.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import type { Content } from '@prismicio/client';
+
+	export let slice: Content.HeroSlice;
+</script>
+
+<section data-slice-type={slice.slice_type} data-slice-variation={slice.variation}>
+	<h1>
+		<span>{slice.primary.title_1}</span>
+		<span>{slice.primary.title_2}</span>
+		<span>{slice.primary.title_3}</span>
+	
+	</h1>
+</section>
