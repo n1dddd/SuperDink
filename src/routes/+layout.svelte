49132896b36a7ece58efa64de067db46cf8a0570
n1dddd <danielseniv@outlook.com>
2024-07-11 02:00:27 -0400

    Add scaffolding for future header/footer sections, add 2 absolute divs at varying Z levels to style the background

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,7 +2,7 @@
 	import { PrismicPreview } from '@prismicio/svelte/kit';
 	import { page } from '$app/stores';
 	import { repositoryName } from '$lib/prismicio';
-	import '@fontsource-variable/jost';
+	import '@fontsource/sarabun';
 	import "../app.css"
 </script>
 
@@ -19,7 +19,20 @@
 		<meta name="twitter:card" content="summary_large_image" />
 	{/if}
 </svelte:head>
+
+<header>
+	Header
+</header>
+
+
 <main>
 	<slot />
 </main>
+
+<div class="background-gradient absolute inset-0 -z-50 max-h-screen" />
+<div class="pointer-events-none absolute inset-0 -z-40 h-full bg-[url('noisetexture.jpg')] opacity-20 mix-blend-soft-light" />
+
+<footer>
+	Footer
+</footer>
 <PrismicPreview {repositoryName} />
