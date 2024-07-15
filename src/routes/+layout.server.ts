a74786f05b6f440a718050a57a9ade2dc327a534
n1dddd <danielseniv@outlook.com>
2024-07-15 04:45:56 -0400

    Add server logic to fetch settings from prismic connection

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,1 +1,14 @@
+import { createClient } from "$lib/prismicio";
+
 export const prerender = 'auto';
+
+export async function load() {
+    const client = createClient()
+
+    const settings = await client.getSingle('settings')
+
+    return {
+        settings
+    }
+}
+
