510f686d456048757552af5bf2aa6310ca76e8b7
n1dddd <danielseniv@outlook.com>
2024-08-12 12:01:07 -0400

    Import the renderer component to canvas

--- src/lib/slices/Hero/App.svelte
+++ src/lib/slices/Hero/App.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
     import { Canvas } from "@threlte/core"
     import Scene from "./Scene.svelte";
+    import Renderer from "$lib/components/Renderer.svelte";
 </script>
 
 <Canvas shadows>
+    <Renderer />
     <Scene />
 </Canvas>
