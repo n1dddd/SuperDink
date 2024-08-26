8502de7b272faf9c3d776d6fe48ed7ee33c70329
n1dddd <danielseniv@outlook.com>
2024-08-25 23:44:14 -0400

    Adjust font sizes

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import {  type Content, isFilled } from "@prismicio/client";
-    import { PrismicLink } from "@prismicio/svelte";
+    import { PrismicImage, PrismicLink } from "@prismicio/svelte";
     import IconX from '~icons/fa6-brands/x-twitter'
     import IconInsta from '~icons/fa6-brands/instagram'
     import Bounded from "./Bounded.svelte";
@@ -11,11 +11,12 @@
 <Bounded as="footer" class="text-slate-100">
     <div class="container mx-auto flex mt-20 flex-col items-center justify-between gap-6 py-8 sm:flex-row">
         <div class="name flex clex-col items-center justify-center gap-x-4 gap-y-2 sm:flex-row sm:justify-self-start">
-            <a href="/" class="text-xl font-extrabold tracking-tighter text-slate-100 transition-color duration-150 hover:text-blue-300">
+            <a href="/" class="text-2xl font-extrabold tracking-tighter text-slate-100 transition-color duration-150 hover:text-blue-300 flex flex-row">
                 {settings.data.name}
+                <PrismicImage field={settings.data.sprdnk_logo} width={30}/>
             </a>
             <span class="hidden text-5xl font-extralight leading-[0] text-slate-400 sm:inline" aria-hidden="true"> / </span>
-            <p class="text-sm text-slate-300">
+            <p class="text-md text-slate-300">
                 © {new Date().getFullYear()} {settings.data.name}    
             </p>
         </div>
@@ -23,10 +24,10 @@
             <ul class="flex items-center gap-1">
                 {#each settings.data.nav_item as {link, label}, index}
                 <li>
-                    <PrismicLink field={link} class="block px-3 py-1 text-base font-bold text-slate-100 transition-colors duration-150 hover:text-blue-300">{label}</PrismicLink>
+                    <PrismicLink field={link} class="block px-3 py-1 text-2xl font-bold text-slate-100 transition-colors duration-150 hover:text-blue-300">{label}</PrismicLink>
                 </li>
                 {#if index < settings.data.nav_item.length - 1}
-                    <span class="text-4xl font-thin leading-[0] text-slate-400" aria-hidden="true">
+                    <span class="text-xl font-thin leading-[0] text-slate-400" aria-hidden="true">
                         /
                     </span>
                 {/if}
@@ -35,12 +36,12 @@
         </nav>
         <div class="socials inline-flex justify-center sm:justify-end">
             {#if isFilled.link(settings.data.x_link)}
-                <PrismicLink field={settings.data.x_link} class="p-2 text-2xl text-slate-300 transform transition-all duration-150 hover:scale-150 hover:text-blue-300" aria-label={settings.data.name + 'on X'}>
+                <PrismicLink field={settings.data.x_link} class="p-2 text-3xl text-slate-300 transform transition-all duration-150 hover:scale-150 hover:text-blue-300" aria-label={settings.data.name + 'on X'}>
                     <IconX />
                 </PrismicLink>
             {/if}
             {#if isFilled.link(settings.data.insta_link)}
-                <PrismicLink field={settings.data.insta_link} class="p-2 text-2xl text-slate-300 transform transition-all duration-150 hover:scale-150 hover:text-blue-300" aria-label={settings.data.name + 'on instagram'}>
+                <PrismicLink field={settings.data.insta_link} class="p-2 text-3xl text-slate-300 transform transition-all duration-150 hover:scale-150 hover:text-blue-300" aria-label={settings.data.name + 'on instagram'}>
                     <IconInsta />
                 </PrismicLink>
             {/if}
