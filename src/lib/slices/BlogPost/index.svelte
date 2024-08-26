900cd8abae47afeb0b3d158f633b9e7809ebbfdd
n1dddd <danielseniv@outlook.com>
2024-08-25 23:46:22 -0400

    Change how date is displayed

--- src/lib/slices/BlogPost/index.svelte
+++ src/lib/slices/BlogPost/index.svelte
@@ -6,34 +6,41 @@
 
 	export let slice: Content.BlogPostSlice;
 
+	const options = {
+		weekday: "long",
+		year: "numeric",
+		month: "long",
+		day: "numeric"
+	}
+
 	const rawPrismicDate = slice.primary.news_time;
 
 	const jsDate = asDate(rawPrismicDate);
 
-	const formattedDate = jsDate?.toLocaleDateString();
+	const formattedTime = jsDate?.toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" });
+
+	const formattedDate = jsDate?.toLocaleDateString('en-US', options);
 </script>
 
 <section data-slice-type={slice.slice_type} data-slice-variation={slice.variation}>
 	<Bounded as="div">
-		<Heading size="md" class="mb-8 gap-y-2" tag="h2">
-			<div>
-				{slice.primary.news_title}
-			</div>
-		</Heading>
-		<div class="flex-row my-6">
-			<Heading size="sm" class="mb-8 gap-y-2" tag="h2">
+		<div class="w-3/4 flex flex-col self-center gap-y-8">
+			<Heading size="md" class="mb-8 gap-y-2" tag="h2">
 				<div>
-					Authored: {formattedDate}
+					{slice.primary.news_title}
 				</div>
 			</Heading>
+			<Heading size="sm" class="mb-8 gap-y-2" tag="h2">
+					<div>
+						{formattedDate} {formattedTime}
+					</div>
+			</Heading>
 			<PrismicImage
-				field={slice.primary.news_image}
-				class="rounded-2xl"
-			/>
-		</div>
-		<div class="flex justify-center">
-			<div class="md:text-lg text-md text-slate-300 w-3/4">
-				<PrismicRichText field={slice.primary.news_description} />
+			field={slice.primary.news_image}
+			class="rounded-2xl"
+		/>
+			<div class="grid md:text-lg text-md text-slate-300 gap-y-8">
+				<PrismicRichText field={slice.primary.news_description}/>
 			</div>
 		</div>
 	</Bounded>
