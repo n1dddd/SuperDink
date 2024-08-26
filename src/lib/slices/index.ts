30ffd475fe01d197020705f49195742f56a10417
n1dddd <danielseniv@outlook.com>
2024-08-25 23:46:47 -0400

    Automated import from slice machine

--- src/lib/slices/index.ts
+++ src/lib/slices/index.ts
@@ -3,11 +3,13 @@
 import BlogPost from './BlogPost/index.svelte';
 import GamesList from './GamesList/index.svelte';
 import Hero from './Hero/index.svelte';
+import MerchTShirt from './MerchTShirt/index.svelte';
 import NewsSlot from './NewsSlot/index.svelte';
 
 export const components = {
 	blog_post: BlogPost,
 	games_list: GamesList,
 	hero: Hero,
+	merch_t_shirt: MerchTShirt,
 	news_slot: NewsSlot
 };
