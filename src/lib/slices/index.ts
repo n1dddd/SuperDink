3cf557c637bcf722e3a656b1de42ef2346343849
n1dddd <danielseniv@outlook.com>
2024-08-28 01:41:43 -0400

    Automatically generated and edited slice machine file

--- src/lib/slices/index.ts
+++ src/lib/slices/index.ts
@@ -5,11 +5,13 @@ import GamesList from './GamesList/index.svelte';
 import Hero from './Hero/index.svelte';
 import MerchTShirt from './MerchTShirt/index.svelte';
 import NewsSlot from './NewsSlot/index.svelte';
+import RosterList from './RosterList/index.svelte';
 
 export const components = {
 	blog_post: BlogPost,
 	games_list: GamesList,
 	hero: Hero,
 	merch_t_shirt: MerchTShirt,
-	news_slot: NewsSlot
+	news_slot: NewsSlot,
+	roster_list: RosterList
 };
