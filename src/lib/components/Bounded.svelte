dab686db965d5a500ace3c0c6dbd7395daa69452
n1dddd <danielseniv@outlook.com>
2024-08-28 02:51:58 -0400

    Change padding to an all around more comfortable values

--- src/lib/components/Bounded.svelte
+++ src/lib/components/Bounded.svelte
@@ -5,7 +5,7 @@
 
 <svelte:element
     this={tag}
-    {...$$restProps} class={`px-4 py-10 md:px-6 md:py-14 lg:py-16 ${$$props.class}`}
+    {...$$restProps} class={`py-6 ${$$props.class}`}
     >
     <div class="flex flex-col mx-auto w-full max-w-7xl">
         <slot/>
